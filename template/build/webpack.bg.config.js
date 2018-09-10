29ba9c69dafbb1f0098e09aae661073e70f17174
Sina Sharifzade <sharifzadesina@gmail.com>
2018-09-10 22:29:19 +0430

    Fixes #8

--- template/build/webpack.bg.config.js
+++ template/build/webpack.bg.config.js
@@ -54,6 +54,9 @@ if (isProduction) {
   config.plugins.push(
     new BabelMinifyWebpackPlugin({}, {
       comments: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
     })
   )
 }
